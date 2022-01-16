f969e1cc3529a0288db69f19bc9211bd44ac43ca
joseph-benoy <josephbenoy03@gmail.com>
2022-01-16 10:51:13 +0530

    changes to header and fixed hero

--- elixir/src/components/Header/Header.jsx
+++ elixir/src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
-import { Navbar,Nav,Container } from "react-bootstrap";
+import { Navbar,Nav,Container ,NavDropdown} from "react-bootstrap";
+import './Header.css';
 const Header = () => {
     return ( 
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="none" expand="lg" sticky="top" id="navbar">
             <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img src="/images/common/logo.png" width="100" height="20"/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                 <Nav className="ms-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#services">Our services</Nav.Link>
+                    <NavDropdown title="Our services" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                    </NavDropdown>
                     <Nav.Link href="#about">About us</Nav.Link>
                     <Nav.Link href="#reviews">Reviews</Nav.Link>
                     <Nav.Link href="#contact">Contact</Nav.Link>
---
+++ elixir/src/pages/Home/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import { Button, Carousel } from "react-bootstrap";
+import './Hero.css';
+const Hero = () => {
+    return ( 
+        <Carousel id="hero-wrapper">
+            <Carousel.Item id="r">
+                <img
+                className="d-block w-100"
+                src="/images/hero/slider-1.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h1>The uae's leading 
+                              snagging & handover
+                              company.</h1>
+                <p>Best snagging, inspection & maintenance services in UAE</p>
+                <Button variant="primary" size="lg">Enquire now</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="/images/hero/slider-2.jpg"
+                alt="Second slide"
+                />
+                <Carousel.Caption>
+                <h1>We see beyond visual inspection</h1>
+                <p>UAE”s most trusted Property Snagging and maintenance services provider and to be investors and Tenants first choice for Property Services</p>
+                <Button variant="primary" size="lg">Enquire now</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+     );
+}
+ 
+export default Hero;
