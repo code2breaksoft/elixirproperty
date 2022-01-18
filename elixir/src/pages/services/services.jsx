fccef6f09988ce27accb1067dff7471bff53766f
joseph-benoy <josephbenoy03@gmail.com>
2022-01-18 17:09:47 +0530

    snagging page added

--- elixir/src/pages/services/services.jsx
+++ elixir/src/pages/services/services.jsx
@@ -1,10 +1,21 @@
 import Footer from "../../components/footer/footer";
 import Header from "../../components/Header/Header";
-
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Container } from "react-bootstrap";
+import Snagging from "./snagging/snagging";
+import Maintenence from "./maintenance/maintenence";
+import Special from "./sepecial/special";
 const Services = () => {
     return ( 
         <>
         <Header/>
+        <Container>
+            <Routes>
+                <Route path="/snagging" element={<Snagging/>}></Route>
+                <Route path="/maintenence" element={<Maintenence/>}></Route>
+                <Route path="/specialized" element={<Special/>}></Route>
+            </Routes>
+        </Container>
         <Footer/>
         </>
      );
---
+++ elixir/src/pages/services/snagging/snagging.jsx
@@ -0,0 +1,49 @@
+import { Row,Tab ,Col,Nav} from "react-bootstrap";
+import MoveIn from "./movein/in";
+import MoveOut from "./moveout/out";
+import Prop from "./prop/prop";
+
+const Snagging = () => {
+    return ( 
+        <section>
+            <Row>
+                <h2>Property snagging</h2>
+                <p>Property Snagging is a service which is opted by the homeowners and investors after purchase of the any new Property i.e Villa, Townhouse, Apartment or any commercial Property.</p>
+            </Row>
+            <Row>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                <Row>
+                    <Col sm={3}>
+                    <Nav variant="pills" className="flex-column">
+                        <Nav.Item>
+                        <Nav.Link eventKey="first">Property snagging</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link eventKey="second">Property move in inspection</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link eventKey="third">Property move out inspection</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    </Col>
+                    <Col sm={9}>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Prop/>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                            <MoveIn/>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                            <MoveOut/>
+                        </Tab.Pane>
+                    </Tab.Content>
+                    </Col>
+                </Row>
+                </Tab.Container>
+            </Row>
+        </section>
+     );
+}
+ 
+export default Snagging;
