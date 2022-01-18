a170804ac4d8055ef2364a479bb5b587c3002e7a
joseph-benoy <josephbenoy03@gmail.com>
2022-01-18 15:14:45 +0530

    footer desktop

--- elixir/src/components/footer/footer.jsx
+++ elixir/src/components/footer/footer.jsx
@@ -6,25 +6,28 @@ const Footer = () => {
     return ( 
         <footer className="footer">
             <Row>
-                <Col lg={4}>
+                <Col lg={4} className="company-wrapper">
                     <img alt="footer-logo" src="/images/common/logo-footer.png"/>
                     <p>We deliver only quality</p>
                 </Col>  
-                <Col lg={4}>
+                <Col lg={4} className="footer-contact-wrapper">
                     <h4>Contact us</h4>
-                    <p><GeoAltFill/>101, Al Mehri Building, Ras Al Khor, Dubai, UAE</p>
-                    <p><TelephoneFill/>+971 4 3302605</p>
+                    <p><GeoAltFill/>&nbsp;&nbsp;101, Al Mehri Building, Ras Al Khor, Dubai, UAE</p>
+                    <p><TelephoneFill/>&nbsp;&nbsp;+971 4 3302605</p>
                 </Col>
-                <Col lg={4}>
+                <Col lg={4} className="footer-social">
                     <h4>Follow us</h4>
-                    <a href="https://www.facebook.com/elixirpropertymanagement"><Facebook/></a>
-                    <a href="https://www.instagram.com/elixirpropertymanagement/"><Instagram/></a>
-                    <a href=""><Twitter/></a>
-                    <a href=""><Youtube/></a>
+                    <a style={{color:"#4267B2"}} href="https://www.facebook.com/elixirpropertymanagement"><Facebook/></a>
+                    <a style={{color:"#bc2a8d"}} href="https://www.instagram.com/elixirpropertymanagement/"><Instagram/></a>
+                    <a style={{color:"#1DA1F2"}} href=""><Twitter/></a>
+                    <a style={{color:"#FF0000"}} href=""><Youtube/></a>
                 </Col>
             </Row>
             <Row>
-
+                <section className="copyright-wrapper">
+                    <p>Copyright © 2022 Elixir Property Snagging. All rights reserved.</p>
+                    <a href="code2break.com">created by Code2Break solutions</a>
+                </section>
             </Row>
         </footer>
      );
