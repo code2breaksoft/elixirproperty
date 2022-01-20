a143262ca709eb74ce1f52cb109fa91da8d8fb8d
joseph-benoy <josephbenoy03@gmail.com>
2022-01-20 22:38:33 +0530

    added scope

--- elixir/src/App.js
+++ elixir/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Services from "./pages/services/services";
 import Faq from "./pages/faq/faq";
 import Blog from "./pages/blog/blog";
+import InspectionScope from "./pages/inspectionScope/inscope";
 
 function App() {
     return ( 
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/services/*" element={<Services/>}></Route>
                 <Route path="/faq" element={<Faq/>}></Route>
                 <Route path="/blog" element={<Blog/>}></Route>
+                <Route path="/inspection-scope" element={<InspectionScope/>}></Route>
               </Routes>
             </Router>
         </div>
