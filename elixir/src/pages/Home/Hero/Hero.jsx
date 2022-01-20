0e7bed4b977875dc7acf1f80dc98076d2163f61a
joseph-benoy <josephbenoy03@gmail.com>
2022-01-20 13:05:22 +0530

    hero fix

--- elixir/src/pages/Home/Hero/Hero.jsx
+++ elixir/src/pages/Home/Hero/Hero.jsx
@@ -4,22 +4,25 @@ import './Hero.css';
 const Hero = () => {
     const [imgA,setimgA] = useState("/images/hero/slider-1.jpg");
     const [imgB,setimgB] = useState("/images/hero/slider-2.jpg");
-    useEffect(()=>{
-        if(window.innerWidth>786){
-            document.getElementById("home").style.marginTop = "-10%";
-        }
-        else{
-            document.getElementById("home").style.marginTop = "-50%";
-            setimgA("/images/hero/2.jpg");
-            setimgB("/images/hero/3.jpg");
-        }
-    },[]);
+
     return ( 
-        <Carousel id="home">
-            <Carousel.Item id="r" >
+        <Carousel id="hero-wrapper" controls={false} >
+                        <Carousel.Item interval={3000}>
                 <img
                 className="d-block w-100"
                 src={imgA}
+                alt="Second slide"
+                />
+                <Carousel.Caption>
+                <h1>We see beyond visual inspection</h1>
+                <p>UAE's most trusted Property Snagging and maintenance services provider.</p>
+                <Button variant="primary" size="lg">Enquire now</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item id="r" >
+                <img
+                className="d-block w-100"
+                src={imgB}
                 alt="First slide"
                 />
                 <Carousel.Caption>
@@ -30,18 +33,7 @@ const Hero = () => {
                 <Button variant="primary" size="lg">Enquire now</Button>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={3000}>
-                <img
-                className="d-block w-100"
-                src={imgB}
-                alt="Second slide"
-                />
-                <Carousel.Caption>
-                <h1>We see beyond visual inspection</h1>
-                <p>UAE's most trusted Property Snagging and maintenance services provider.</p>
-                <Button variant="primary" size="lg">Enquire now</Button>
-                </Carousel.Caption>
-            </Carousel.Item>
+
             </Carousel>
      );
 }
