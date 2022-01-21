c416a8f3fd667e714d87b3b2378159004a5e4d1c
joseph-benoy <josephbenoy03@gmail.com>
2022-01-21 09:35:22 +0530

    added modal base

---
+++ elixir/src/components/modal/modal.jsx
@@ -0,0 +1,46 @@
+import { Form, Modal } from "react-bootstrap";
+
+const FormModal = ({show}) => {
+    const handleClose = ()=>{show=false;}
+    return ( 
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Register your interest</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+            <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control type="tel" placeholder="Enter phone number" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Property details</Form.Label>
+                <FormControl as="textarea" placeholder="Enter your property details" />
+            </Form.Group>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Book a free qoute
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     );
+}
+ 
+export default FormModal;
