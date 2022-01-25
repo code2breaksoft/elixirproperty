31e1b84c998e76e298c211127b30f29fd3dc4245
joseph-benoy <josephbenoy03@gmail.com>
2022-01-25 23:31:50 +0530

    fixed navdropdown

--- elixir/src/components/Header/Header.jsx
+++ elixir/src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Navbar,Nav,Container ,NavDropdown,Button} from "react-bootstrap";
 import './Header.css';
 import { List } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 import logo from "../../images/common/elixir_logo.png"
+import { useLocation } from "react-router-dom";
 const Header = ({home}) => {
+    const location = useLocation();
+    const [drop,setDrop] = useState(false);
     const nav = useNavigate();
     const changePage = useCallback((path)=>{
         nav(path);
         var elmnt = document.getElementById(path.split('#').pop());
         elmnt.scrollIntoView();
     },[]);
+    const toggle = ()=>{
+        setDrop(false);
+    }
+    const [open,setOpen] = useState(false);
+    useEffect(()=>{
+        if(!drop){
+            setOpen(false);
+        }
+        else{
+            setOpen(true);
+        }
+    },[location])
+
     return ( 
         <>
                     <Navbar bg={"light"} expand="lg" id="navbar" sticky="top" collapseOnSelect>
@@ -24,16 +40,16 @@ const Header = ({home}) => {
                 <Nav className="ms-auto">
                     <Nav.Link className="under" eventKey="0" onClick={()=>{changePage("/#home")}} >HOME</Nav.Link>
                     <Nav.Link className="under" eventKey="1" onClick={()=>{changePage("/#about")}}>ABOUT US</Nav.Link>
-                    <NavDropdown title="OUR SERVICES" id="basic-nav-dropdown">
-          <NavDropdown.Item  eventKey="2"  onClick={()=>{changePage("/services/property-handover-inspection")}}>Property Handover Inspection
-</NavDropdown.Item>
-          <NavDropdown.Item  eventKey="3"  onClick={()=>{changePage("/services/property-resale-inpection")}}>Property Resale Inspection
-</NavDropdown.Item>
-          <NavDropdown.Item  eventKey="4"  onClick={()=>{changePage("/services/11-month-defect-liablity-inspection")}} >11 Month-Defect Liability Inspection
-</NavDropdown.Item>
-          <NavDropdown.Item  eventKey="5"  onClick={()=>{changePage("/services/property-move-in-out-inspection")}}>Property move in/out inspection
-</NavDropdown.Item>
-        </NavDropdown>                    
+                    <NavDropdown show={open} onClick={()=>setOpen(!open)} title="OUR SERVICES" id="basic-nav-dropdown">
+                            <NavDropdown.Item eventKey="2"  onClick={()=>{changePage("/services/property-handover-inspection");setDrop(false);}}>Property Handover Inspection
+                    </NavDropdown.Item>
+                            <NavDropdown.Item   eventKey="3"  onClick={()=>{changePage("/services/property-resale-inpection");setDrop(false);}}>Property Resale Inspection
+                    </NavDropdown.Item>
+                            <NavDropdown.Item   eventKey="4"  onClick={()=>{changePage("/services/11-month-defect-liablity-inspection");setDrop(false);}} >11 Month-Defect Liability Inspection
+                    </NavDropdown.Item>
+                            <NavDropdown.Item  eventKey="5"  onClick={()=>{changePage("/services/property-move-in-out-inspection");setDrop(false);}}>Property move in/out inspection
+                    </NavDropdown.Item>
+                            </NavDropdown>                    
                     <Nav.Link className="under" eventKey="6" onClick={()=>{changePage("/#tools")}}>TOOLS</Nav.Link>
                     <Nav.Link className="under" eventKey="7" onClick={()=>{changePage("/inspection-scope")}}>INSPECTION SCOPE</Nav.Link>
                     <Nav.Link className="under" eventKey="8" onClick={()=>{changePage("/#contact")}}>CONTACT&nbsp;&nbsp;</Nav.Link>
