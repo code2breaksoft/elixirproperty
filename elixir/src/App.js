eb01a5c9f26e19858d45bcdad067b4974093718c
joseph-benoy <josephbenoy03@gmail.com>
2022-01-25 22:58:33 +0530

    fixed scroll

--- elixir/src/App.js
+++ elixir/src/App.js
@@ -6,11 +6,13 @@ import Blog from "./pages/blog/blog";
 import InspectionScope from "./pages/inspectionScope/inscope";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ScrollToTop from "./components/scrollToTop/scrollToTop";
 function App() {
   const [countries,setCountries] = useState();
     return ( 
     < div className = "App" >
             <Router>
+              <ScrollToTop>
               <Routes>
                 <Route path="/" element={<Home country={countries}/>}></Route>
                 <Route path="/services/*" element={<Services/>}></Route>
@@ -18,6 +20,7 @@ function App() {
                 <Route path="/blog" element={<Blog/>}></Route>
                 <Route path="/inspection-scope" element={<InspectionScope/>}></Route>
               </Routes>
+              </ScrollToTop>
             </Router>
         </div>
     );
